a45f3a72f5124ef028e66e8f8ee4a62a3f8dc6ef
Pavle Smiga <smigapav@fel.cvut.cz>
2024-12-10 10:36:23 +0100

    feat: changed project structure and added spring dependencies

---
+++ src/main/java/cz/pps/auto_dl_be/AutoDlBeApplication.java
@@ -0,0 +1,13 @@
+package cz.pps.auto_dl_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoDlBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoDlBeApplication.class, args);
+	}
+
+}
