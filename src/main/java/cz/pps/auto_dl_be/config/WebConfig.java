2b18a95573bebe31db3baea1db13e162a239591f
Pavel Smiga <smigapav@fel.cvut.cz>
2025-01-22 14:47:19 +0100

    fix: added CORS configuration

---
+++ src/main/java/cz/pps/auto_dl_be/config/WebConfig.java
@@ -0,0 +1,25 @@
+package cz.pps.auto_dl_be.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://65.21.94.175:50000/", "http://localhost:50000")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
